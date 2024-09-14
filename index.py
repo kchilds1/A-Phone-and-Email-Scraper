b937ea9b34ad4c984c11dae5146291238849aeb2
kchilds1 <kenneth.childs20@gmail.com>
2024-09-14 18:25:48 -0500

    Created email regular expression variable

--- index.py
+++ index.py
@@ -6,11 +6,20 @@ import re, pyperclip
 phoneRegex = re.compile(r'''
 #types of numbers I want ex. 111-111-1111, 111-1111, (111) 111-1111, or 111-1111 ext. 12345, x12345
 
-((\d\d\d)|(\(\d\d\d)))?   # area code (optional and could appear 0 or 1 times)
-(\s|-)    #first separator
-\d\d\d    #first 3 digits
--    #second separator
-\d\d\d\d    #last 4 digits
-((ext(\.)?\s)|x)   # extension (optional where it could say ext with or without . or could contain x )
-(\d{2,5}))? #extension could be 2 to 5 digits         
-           ''', re.VERBOSE)
+    ((\d\d\d)|(\(\d\d\d)))?   # area code (optional and could appear 0 or 1 times)
+    (\s|-)    #first separator
+    \d\d\d    #first 3 digits
+    -    #second separator
+    \d\d\d\d    #last 4 digits
+    ((ext(\.)?\s)|x)   # extension (optional where it could say ext with or without . or could contain x )
+    (\d{2,5}))? #extension could be 2 to 5 digits         
+               ''', re.VERBOSE)
+
+# Create a regex for email addresses
+emailRegex = re.compile(r'''
+# something@something.com,something_something@something.com, something.something@something.com,something+something@something.com
+
+    [a-zA-Z0-9_.+?]+ #name part-this can contain lowercase,uppercase, numbers, and the following symbols _.+?
+    @#@ symbol
+    [a-zA-Z0-9_.+?]+# Domain name part-this can contain lowercase,uppercase, numbers, and the following symbols _.+?              
+    ''', re.VERBOSE)
