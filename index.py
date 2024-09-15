621fcb84b69b6853bbb5e519de5cec654a87a114
kchilds1 <kenneth.childs20@gmail.com>
2024-09-14 19:30:06 -0500

    added string to text and printed results.  I had one to many ) for the optional area code

--- index.py
+++ index.py
@@ -5,14 +5,14 @@ import re, pyperclip
 # Create a regex for phone numbers
 phoneRegex = re.compile(r'''
 #types of numbers I want ex. 111-111-1111, 111-1111, (111) 111-1111, or 111-1111 ext. 12345, x12345
-
-    ((\d\d\d)|(\(\d\d\d)))?   # area code (optional and could appear 0 or 1 times)
+(
+    ((\d{3})|(\(\d{3}))?   # area code optional and could appear 0 or 1 times
     (\s|-)    #first separator
-    \d\d\d    #first 3 digits
+    \d{3}    #first 3 digits
     -    #second separator
-    \d\d\d\d    #last 4 digits
-    ((ext(\.)?\s)|x)   # extension (optional where it could say ext with or without . or could contain x )
-    (\d{2,5}))? #extension could be 2 to 5 digits         
+    \d{4}   #last 4 digits
+    ((ext(\.)?\s)|x)?   # extension optional where it could say ext with or without . or could contain x 
+    (\d{2,5})?) #extension could be 2 to 5 digits         
                ''', re.VERBOSE)
 
 # Create a regex for email addresses
@@ -25,8 +25,13 @@ emailRegex = re.compile(r'''
     ''', re.VERBOSE)
 
 # Get the text off the clipboard
-text = pyperclip.paste() # whatever I have in my clipboard will be pasted to text
+text = 'Clinton Hernandez chernandez16@yahoo.com 303-606-9242 Sylvester Goodman sylvesterg@comcast.net 419-691-5429 Efren Daniels edaniels@comcast.net 740-228-1291 '
+#pyperclip.paste() # whatever I have in my clipboard will be pasted to text
 
 # Extract the email/phone from this text
 extractedPhone = phoneRegex.findall(text)
 extractedEmail = emailRegex.findall(text)
+
+print(extractedEmail)
+print(extractedPhone)
+
